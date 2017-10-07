206b806afdb882ad93f9cd77b19346977677af84
J. Cohen <hostelmarketing@gmail.com>
2017-10-06 23:24:10 -0700

    add migration info

--- db/migrations/20171006231106_create_users_and_todos_tables.js
+++ db/migrations/20171006231106_create_users_and_todos_tables.js
@@ -1,8 +1,22 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('users', table => {
+        table.increments();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('todos', table => {
+        table.increments();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+        table.string('title').notNullable();
+        table.boolean('completed').notNullable().defaultTo(false);
+        // Foreign key
+        table.integer('user_id').references('id').inTable('users');
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTables('todos').dropTables('users');
 };
