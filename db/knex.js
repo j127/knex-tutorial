1de715321ab54cf617526518c59ca387bdef6061
J. Cohen <hostelmarketing@gmail.com>
2017-10-08 19:31:53 -0700

    Add logging to knex.js

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,12 @@
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
+
+// LOG ============================================
+console.log(`loading environment: ${environment}`);
+
+if (environment === 'development') {
+    console.log(`with settings:\n${JSON.stringify(config)}`);
+}
+// ================================================
+
 module.exports = require('knex')(config);
