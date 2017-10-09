69930e36dda9553836e88df44d2e362ffe9df28e
J. Cohen <hostelmarketing@gmail.com>
2017-10-08 19:11:18 -0700

    Update seeds files

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -5,8 +5,8 @@ return knex('users').del()
     .then(function () {
         // Inserts seed entries
         return knex('users').insert([
-            {id: 1, name: 'User 1', email: '1@example.com'},
-            {id: 2, name: 'User 2', email: '2@example.com'},
-            {id: 3, name: 'User 3', email: '3@example.com'},
+            {id: 1, name: 'Alice', email: 'alice@example.com'},
+            {id: 2, name: 'Bob', email: 'bob@example.com'},
+            {id: 3, name: 'Charlie', email: 'charlie@example.com'},
         ]);
     });
--- db/seeds/02_todos.js
+++ db/seeds/02_todos.js
@@ -6,15 +6,15 @@ exports.seed = (knex, Promise) => {
         .then(() => {
             // Inserts seed entries
             return knex('todos').insert([
-                { title: 'do something',
-                  user_id: 3 },
-                { title: 'go shopping',
-                  user_id: 2 },
-                { title: 'exercise',
-                  user_id: 2 },
                 { title: 'write a computer program',
                   user_id: 1 },
                 { title: 'cook food and eat it',
                   user_id: 1 },
+                { title: 'go shopping',
+                  user_id: 2 },
+                { title: 'exercise',
+                  user_id: 2 },
+                { title: 'do something',
+                  user_id: 3 },
             ]);
         });
